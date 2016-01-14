75b4c93fb7ac958c6a06ab4bf053d32d1005d20d
Bill Normandin <bill@normandindev.net>
2016-01-13 20:25:14 -0600

    More debugging, added pokeyworks and application object

--- hgtools_gtk.py
+++ hgtools_gtk.py
@@ -4,6 +4,7 @@ from lib import *
 #from lib.dedupe import *
 from gi.repository import Gtk
 import time, logging, getpass
+import traceback
 import argparse
 import pokeyworks.pokeyworks as fw
 
@@ -14,37 +15,85 @@ PURPLE_CONV_TYPE_IM=1
 USER_SELECTION=0
 	
 # Create logger, default to logging.DEBUG
-hgt_logger = utils.setup_logger('hgtools_gtk.py', logging.INFO, LOG_PATH)
+hgt_logger = utils.setup_logger('hgt_logger', logging.DEBUG, LOG_PATH)
 	
 #*****************************END LOGGING*******************************
 
-def main():
+class HGToolsApplication(object):
 	
-	parser = argparse.ArgumentParser()
-	
-	parser.add_argument("-d", "--debug", help="Enable Debugging mode",
-                    action="store_true")
-	
-	args = parser.parse_args()
-	
-	config = fw.PokeyConfig('./hgt.conf')
-	config.log_path = ('./tmp/{!s}.log'.format(timepart))
-	config.timepart = time.strftime("%Y%m%d")
-	config.env_user = getpass.getuser()
-	config.favicon = utils.hgt_resource_path("./resources/images/snappyfav.png").replace('lib/','')
-	config.user_level = ''
-	
-	global favicon
-	try:
-		gui.gtk_style(CSS_PATH)
-		win = gui.MainWindow(ENV_USER, favicon, logging.DEBUG)
-		win.connect("delete-event", Gtk.main_quit)
-		hgt_logger.debug('[*] Showing Window')
-		win.show_all()
-		hgt_logger.debug('[*] Entering Gtk.main()')
-		Gtk.main()
-	except (KeyboardInterrupt, SystemExit):
-		Gtk.main_quit()
+	def __init__(self):
+		hgt_logger.debug('[*] Begin HGToolsApplication.__init__()')
+		
+		try:
+			# Set up command-line argument handler
+			parser = argparse.ArgumentParser()
+		
+			parser.add_argument("-d", "--debug", help="Enable Debugging mode",
+								action="store_true")
+			
+			# Capture command-line arguments
+			self.args = parser.parse_args()
+			
+			# Initialize and populate config values
+			
+			# File Values
+			config = fw.PokeyConfig('./hgt.conf')
+			# Other Values
+			config.log_path = ('./tmp/{!s}.log'.format(timepart))
+			config.timepart = time.strftime("%Y%m%d")
+			config.env_user = getpass.getuser()
+			
+			# Set favicon path using the utils hgt_resource_path method
+			# and the config file's relative path location
+			self.favicon = utils.hgt_resource_path(config.favicon).replace('lib/','')
+			
+			# Initialize to empty(new) user level until DB is checked
+			config.user_level = ''
+			
+			# Finalize Config
+			self.config = config
+		except:
+			raise
+			sys.exit(1)
+		else:
+			self.execute()
+		finally:
+			sys.exit(0)
+		
+	def execute(self):
+		""" Executes the main application, window, and Gtk Loop """
+		hgt_logger.debug('[*] Begin HGToolsApplication.execute()')
+		
+		try:
+			# Initiate stle
+			gui.gtk_style(CSS_PATH)
+			
+			# Instantiate Gtk window
+			self.win = gui.MainWindow(ENV_USER, favicon, logging.DEBUG)
+			self.win.connect("delete-event", Gtk.main_quit)
+			
+			# Show window
+			hgt_logger.debug('[*] Showing Window')
+			self.win.show_all()
+			
+			# Entering main_loop
+			hgt_logger.debug('[*] Entering Gtk.main()')
+			Gtk.main()
+			
+		except (KeyboardInterrupt, SystemExit):
+			# Capture and exit upon sys.exit() and CTRL+C events
+			hgt_logger.debug('[*] Exit command detected!')
+			Gtk.main_quit()
+			self.failsafe()
+			
+		except:
+			raise
+			
+	def failsafe():
+		""" Failsafe to close connections if open, and other cleanup """
+		hgt_logger.debug('[*] Failsafe engaged!')
+		
+		pass
 	
 if __name__ == '__main__':
-	main()
+	HGToolsApplication()
